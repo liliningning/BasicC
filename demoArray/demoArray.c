7aae711f1fc79cd777029fc167136f9948fbdb83
liliningning <2914352881@qq.com>
2023-12-08 15:58:13 +0800

    [新增]新增数组

---
+++ demoArray/demoArray.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 10
+
+/*数组
+  1. 一断连续的存储空间
+  2. 它里面存放的数据内型是相同的
+
+
+
+*/
+int main()
+{
+    int array[BUFFER_SIZE] = { 0 };
+
+    for (int idx = 0; idx < BUFFER_SIZE; idx++)
+    {
+        printf("array[%d] = %d\n", idx, array[idx]);
+    }
+
+#if 0
+    // 地址
+    // 数组的首地址和数组第一个元素的地址是一样的
+    printf("%p\n", array);
+    printf("%p\n", &array[0]);
+    printf("%p\n", &array[1]);
+    printf("%p\n", &array[9]);
+#endif
+
+#if 0
+    // 清楚脏数据
+    array[BUFFER_SIZE];
+    memset(array, 0, sizeof(array));
+    printf("............\n");
+    for (int idx = 0; idx < BUFFER_SIZE; idx++)
+    {
+        printf("array[%d] = %d\n", idx, array[idx]);
+    }
+#endif
+    return 0;
+}

06f34a4d18b7abf533e9e97155791d515a5dfe28
liliningning <2914352881@qq.com>
2023-12-08 21:30:28 +0800

    [新增] 新增字符数组的表达式

---
+++ demoArray/demoCharArray.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE 10
+int main()
+{
+
+#if 0
+    // 获取多少个元素 ： 数组的长度 除以每个元素的长度
+    char array[BUFFER_SIZE] = {0};
+    int arrayLen = sizeof(array);
+    printf("arraayLen%d\n", arrayLen);
+
+    int elemtLen = sizeof(array[0]);
+    printf("elemtLen%d\n", elemtLen);
+
+    int length = arrayLen / elemtLen;
+    printf("length%d\n", length);
+#endif
+#if 1
+
+    char pNum = 'a';
+    printf("pNum%c\n", pNum);
+
+    // 指针变量 定义第一个是P开头
+
+    char *ptr = "hello world";
+    printf("ptr:%s\n", ptr); // %s打印字符串
+
+    char buffer[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
+    int buflen = sizeof(buffer);
+    printf("buflen%d   buffer%s \n", buflen, buffer);
+
+#endif
+
+    /*   数组以双引号定义 会默认加上 '\0 '  
+                以单引号 就没有 '\0'                    */
+
+    // char buffer1[] = "helloworld"; // 默认 '\0'
+    // char buffer2[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
+    // char buffer3[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
+
+    // int  buflen1 = sizeof(buffer1);
+    // int  buflen2 = sizeof(buffer2);
+
+    // printf("buflen1:  %d ,  buflen2: %d\n", buflen1, buflen2);
+    // printf("&buffer1: %p\n" , buffer1);
+    // printf("&buffer2: %p\n" , buffer2);
+
+    // printf("&buffer1: %s\n", buffer1);
+    // printf("&buffer2: %s\n", buffer2);
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ demoArray/demoString.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+
+
+const  int g_num = 100;
+int main()
+{
+    char buffer1[] = "helloworld";
+    buffer1[0] = 'H';
+    printf("buffer1: %s\t  buffer1[0] %c\n", buffer1,buffer1[0]);
+
+
+
+//字符串 不能更改 因为字符串是常量 它存放的区域是全局区
+    char * ptr = "helloworld";
+
+    ptr[0] = 'H';
+    printf("ptr[0]: %c\n",ptr[0]);
+    printf("ptr[0]: %c\t  *(ptr+1): %c\n",*(ptr + 0), (*(ptr +1)));
+    
+
+    
+    
+    
+    return 0;
+}
